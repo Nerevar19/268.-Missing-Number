63ab61248a14737f019b2f2ec8c174e8f8b7820a
Artem Kopytko <artem.kop19@gmail.com>
2023-08-05 15:07:51 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+namespace _268._Missing_Number
+{
+    internal class Program
+    {
+        static public int MissingNumber(int[] nums)
+        {
+            
+            Array.Sort(nums);
+            
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != i) return i;
+            }
+            return nums.Length;
+        }
+        static void Main(string[] args)
+        {
+            int[] nums = { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
+
+            Console.WriteLine(MissingNumber(nums));
+        }
+    }
+}
